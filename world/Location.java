9e28b39fa13380310cbd566bfce4838318b563b9
jonathanleemn <jonathanleemn@gmail.com>
2016-11-16 03:11:06 -0600

    Create Location.java

---
+++ world/Location.java
@@ -0,0 +1,33 @@
+package world;
+
+public class Location
+{
+	int row, col;
+	
+	public Location(int row, int col)
+	{
+		this.row = row;
+		this.col = col;
+	}
+
+	public int getRow()
+	{
+		return row;
+	}
+
+	public void setRow(int row)
+	{
+		this.row = row;
+	}
+
+	public int getCol()
+	{
+		return col;
+	}
+
+	public void setCol(int col)
+	{
+		this.col = col;
+	}
+	
+}
