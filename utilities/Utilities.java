bee92e347f3c3ae8bc321d3428ac531d6c79c6df
Mark Hawes <chaosofazure@yahoo.com>
2016-11-30 13:58:03 -0600

    Add files via upload

--- utilities/Utilities.java
+++ utilities/Utilities.java
@@ -1,5 +1,25 @@
 package utilities;
 
+import java.util.Random;
+import entities.Entity;
+
 public class Utilities {
+
+	// attack with ability modifier
+		public int Attack(Entity attacker, Entity defender) {
+			Random rand = new Random();
+			int chance = 0;
+
+			chance = rand.nextInt(attacker.getHitRating() - defender.getDodgeChance());
+			
+			try{if (rand.nextInt(chance) > 0)
+				return (attacker.getAttack() - defender.getDefense());
+			else
+				return 0;
+			}catch (NullPointerException error){
+				// catches errors and just returns 0 damage
+				return 0;
+			}
+		}
 
 }
