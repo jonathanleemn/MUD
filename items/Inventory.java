1b1355377c78fb9e3f9a7c76e94bb7b45524d086
Mark Hawes <chaosofazure@yahoo.com>
2016-11-30 14:05:49 -0600

    Add files via upload

--- items/Inventory.java
+++ items/Inventory.java
@@ -1,126 +1,87 @@
-package items;
+package inventory;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
-public class Inventory
-{
-	@Override
-	public String toString()
-	{
-		return "Inventory [equippableItems=" + equippableItems + ", itemsEquipped=" + Arrays.toString(itemsEquipped)
-				+ ", consumableItems=" + consumableItems + ", miscItems=" + miscItems + "]";
-	}
+import utilities.Constants;
 
-	private ArrayList<Equippable> equippableItems;
-	private Equippable[] itemsEquipped;
+public class Inventory {
+	private ArrayList<Equipable> equipableItems;
 	private ArrayList<Consumable> consumableItems;
-	private ArrayList<MiscItem> miscItems;
-	
-	public Inventory()
-	{
-		equippableItems = new ArrayList<Equippable>();
-		consumableItems = new ArrayList<Consumable>();
-		miscItems = new ArrayList<MiscItem>();
-		itemsEquipped = new Equippable[2];
+	private Equipable[] equippedItems;
+
+	public Inventory(){
+		equipableItems=new ArrayList<Equipable>();
+		consumableItems=new ArrayList<Consumable>();
+		equippedItems=new Equipable[Constants.PLAYER_EQUIP_SLOTS];
 	}
-	public void addMiscItemToInventory(MiscItem item)
-	{
-		miscItems.add(item);
+
+	public void addEquipable(Equipable item){
+		equipableItems.add(item);
 	}
-	
-	public MiscItem removeMiscItemFromInventory(int index)
-	{
-		return miscItems.remove(index);		
-	}
-	
-	public void addEquippableToInventory(Equippable item)
-	{
-		equippableItems.add(item);
-	}
-	
-	public Equippable removeEquippableFromInventory(int index)
-	{
-		return equippableItems.remove(index);
-	}
-	
-	public void addConsumableToInventory(Consumable item)
-	{
+
+	public void addConsumable(Consumable item){
 		consumableItems.add(item);
 	}
-	
-	public void equipItem(int item)
-	{
-		Equippable toEquip = equippableItems.remove(0);
-		if(toEquip instanceof Armor)
-		{
-			itemsEquipped[0] = toEquip;
-		}
-		else if(toEquip instanceof Weapon)
-		{
-			itemsEquipped[1] = toEquip;
-		}
+
+	public Equipable removeEquipable(int index){
+		return equipableItems.remove(index);
 	}
-	
-	public Consumable removeConsumable(int index)
-	{
+
+	public Consumable removeConsumable(int index){
 		return consumableItems.remove(index);
 	}
-	
-	public ArrayList<Consumable> getConsumableItems()
-	{
+
+	//equipItem
+	public void equipItem(int item){
+		Equipable toEquip=equipableItems.remove(item);
+		if(toEquip instanceof Armor){
+			equippedItems[0]=toEquip;
+		}
+		else if(toEquip instanceof Weapon)
+			equippedItems[1]=toEquip;
+		else
+			equippedItems[2]=toEquip;
+	}
+
+	public void unequipItem(int index){
+		if(equippedItems[index]!=null){
+			equipableItems.add(equippedItems[index]);
+			equippedItems[index]=null;
+		}
+	}
+
+	//getters and setters
+	public ArrayList<Equipable> getEquipableItems() {
+		return equipableItems;
+	}
+
+	public void setEquipableItems(ArrayList<Equipable> equipableItems) {
+		this.equipableItems = equipableItems;
+	}
+
+	@Override
+	public String toString() {
+		return "Inventory [equipableItems=" + equipableItems + "]";
+	}
+
+
+
+	public ArrayList<Consumable> getConsumableItems() {
 		return consumableItems;
 	}
 
-	public void setConsumableItems(ArrayList<Consumable> consumableItems)
-	{
+
+
+	public void setConsumableItems(ArrayList<Consumable> consumableItems) {
 		this.consumableItems = consumableItems;
 	}
 
-	public void unequipItem(int index)
-	{
-		if(itemsEquipped[index] != null)
-		{
-			equippableItems.add(itemsEquipped[index]);
-			itemsEquipped[index] = null;
-		}
+	public Equipable[] getEquippedItems() {
+		return equippedItems;
 	}
 
-	public ArrayList<Equippable> getEquippableItems()
-	{
-		return equippableItems;
+	public void setEquippedItems(Equipable[] equippedItems) {
+		this.equippedItems = equippedItems;
 	}
 
-	public void setEquippableItems(ArrayList<Equippable> equippableItems)
-	{
-		this.equippableItems = equippableItems;
-	}
-
-	public Equippable[] getItemsEquipped()
-	{
-		return itemsEquipped;
-	}
-
-	public void setItemsEquipped(Equippable[] itemsEquipped)
-	{
-		this.itemsEquipped = itemsEquipped;
-	}
-	
-	public int getAttackBonus(){
-		int total = 0;
-		
-		for (int i = 0; i < itemsEquipped.length; i++){
-			total += itemsEquipped[i].getAttBonus();
-		}
-		return total;
-	}
-	
-	public int getDefenseBonus(){
-		int total = 0;
-		
-		for (int i = 0; i < itemsEquipped.length; i++){
-			total += itemsEquipped[i].getDefBonus();
-		}
-		return total;
-	}
 }
